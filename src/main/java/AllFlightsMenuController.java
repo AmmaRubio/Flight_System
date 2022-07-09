0404ceecbd4e24cba285b1017c10c8d39ffa41e4
ge64kem <ge64kem@mytum.de>
2022-07-09 17:46:05 +0200

    Front end for Main Menu and All Flights Menu

--- src/main/java/AllFlightsMenuController.java
+++ src/main/java/AllFlightsMenuController.java
@@ -1,18 +1,47 @@
+import MockClasses.Flight;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.scene.control.TableView;
 
+
+import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
 
-public class AllFlightsMenuController {
+public class AllFlightsMenuController implements Initializable {
     private final String MAINMENU_TITLE = "All flights Menu";
-    @FXML
-    Label label;
 
+    @FXML private Label label;
+    @FXML private TableView table;
+    @FXML private TableColumn<Flight,String> id;
+    @FXML private TableColumn<Flight,String> company;
+    @FXML private TableColumn<Flight, Hyperlink> departure;
+    @FXML private TableColumn<Flight,String> destination;
+    @FXML private TableColumn<Flight,String> date;
+    @FXML private TableColumn<Flight,String> departureTime;
+    @FXML private TableColumn<Flight,String> destinationTime;
+    @FXML private TableColumn<Flight,String> duration;
+
+    //Mock data: TODO: implement normal data. Only this should be changed
+    private final ObservableList<Flight> mockData =
+            FXCollections.observableArrayList(new Flight(1L,"Munich","Frankfurt",
+                    LocalDate.of(2022,12,7),LocalTime.of(12,00),
+                    LocalTime.of(14,00), "LH"));
 
     public void switchToMainMenu(ActionEvent event){
         try{
@@ -23,6 +52,35 @@ public class AllFlightsMenuController {
             Scene scene = new Scene(parent);
             stage.setScene(scene);
             stage.setTitle(MAINMENU_TITLE);
+            stage.show();
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        id.setCellValueFactory(new PropertyValueFactory<Flight, String>("id"));
+        company.setCellValueFactory(new PropertyValueFactory<Flight, String>("company"));
+        departure.setCellValueFactory(new PropertyValueFactory<Flight, Hyperlink>("departure"));
+        destination.setCellValueFactory(new PropertyValueFactory<Flight, String>("destination"));
+        date.setCellValueFactory(new PropertyValueFactory<Flight, String>("date"));
+        departureTime.setCellValueFactory(new PropertyValueFactory<Flight, String>("departureTime"));
+        destinationTime.setCellValueFactory(new PropertyValueFactory<Flight, String>("destinationTime"));
+        duration.setCellValueFactory(new PropertyValueFactory<Flight, String>("duration"));
+        table.getItems().setAll(mockData);
+
+    }
+
+    public void switchToCityMenu(ActionEvent event){
+        try{
+            Parent parent = FXMLLoader.load(getClass().getResource("CityMenu"));
+
+
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.setTitle("City Menu"); //TODO implement name of city as city title
             stage.show();
         } catch (Exception e){
             throw new RuntimeException(e);
---
+++ src/main/java/MockClasses/Flight.java
@@ -0,0 +1,107 @@
+package MockClasses;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Flight {
+    public Flight(Long id, String departure, String destination, LocalDate date,
+                  LocalTime departureTime, LocalTime destinationTime,
+                  String company) {
+        this.id = id;
+        this.departure = departure;
+        this.destination = destination;
+        this.date = date;
+        this.departureTime = departureTime;
+        this.destinationTime = destinationTime;
+        this.company = company;
+        this.flightDuration = Duration.between(departureTime,destinationTime);
+    }
+
+    private Long id;
+    private String departure;
+    private String destination;// TODO: change Class to city
+    private LocalDate date;
+    private LocalTime departureTime; // TODO: change naming in UML class diagram
+    private LocalTime destinationTime;
+    private String company;
+    private Duration flightDuration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalTime getDestinationTime() {
+        return destinationTime;
+    }
+
+    public void setDestinationTime(LocalTime destinationTime) {
+        this.destinationTime = destinationTime;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Duration getFlightDuration() {
+        return flightDuration;
+    }
+
+    public void setFlightDuration(Duration flightDuration) {
+        this.flightDuration = flightDuration;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", departure='" + departure + '\'' +
+                ", destination='" + destination + '\'' +
+                ", date=" + date +
+                ", departureTime=" + departureTime +
+                ", destinationTime=" + destinationTime +
+                ", company='" + company + '\'' +
+                ", flightDuration=" + flightDuration +
+                '}';
+    }
+}
